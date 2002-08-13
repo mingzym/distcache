1f153cc209c8c4a5e288fe7d1a43a342b438fa24
geoffthorpe <geoffthorpe>
2002-08-13 05:16:34 +0000

    Given there is a "bootstrap.sh" script, the purpose of this should be relatively obvious.

---
+++ unbootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# This script handles unwinding the GNU autotools.
+# 
+# Running this script should (hopefully) clean up any/all gunge left lying
+# around from the autotools - ie. leaving you with something resembling as
+# closely as possible the state of CVS.
+
+if [ -f Makefile ]; then
+	make distclean
+fi
+
+# Zilch any "*.in" files except configure.in, and zilch configure too
+find . -type f -name "*.in" | egrep -v "configure.in$" | xargs rm -f
+rm -f configure
+
+# Zilch miscellaneous generated files
+find . -type f -name "aclocal.m4*" | xargs rm -f
+rm -f config.*
+rm -f conftest*
+rm -f stamp-h.in
+rm -f aclocal.m4
+rm -rf autom4te*.cache
+
+# If the config/ directory exists, blow it away
+if [ -d config ]; then
+	rm -rf config/
+fi
+
+# Residual backup files and what-not
+find . -type f -name "*~" | xargs rm -f
+find . -type f -name "*.bak" | xargs rm -f
+find . -type f -name ".#*" | xargs rm -f
+
+# Get rid of packaging byproducts
+rm -f .packagelist
+rm -rf .temppackagedir
+
+# Clean autoconf stuff that autoconf itself doesn't clean
+rm -f config.status
+
