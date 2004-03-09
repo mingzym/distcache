42ccd7097f2eceb4639d67a6197877d6ac6def27
geoffthorpe <geoffthorpe>
2004-03-09 06:29:29 +0000

    More libnal overhaul. Modifies the select()-based selector implementation to the new interface and I/O model.

--- libnal/sel_select.c
+++ libnal/sel_select.c
@@ -25,6 +25,16 @@
 #include "nal_internal.h"
 #include <libsys/post.h>
 
+#ifndef HAVE_SELECT
+
+/* If we don't build select() support, return a NULL vtable */
+const NAL_SELECTOR_vtable *sel_fdselect(void)
+{
+	return NULL;
+}
+
+#else
+
 /* These are the structure types we use */
 typedef struct st_sel_item {
 	fd_set reads;
@@ -32,40 +42,174 @@ typedef struct st_sel_item {
 	fd_set excepts;
 	int max;
 } sel_item;
+typedef struct st_sel_obj {
+	union {
+		NAL_CONNECTION *conn;
+		NAL_LISTENER *listener;
+	} obj;
+	unsigned char what; /* 0==unused, 1==conn, 2==listener */
+} sel_obj;
 typedef struct st_sel_ctx {
 	/* The result of a select */
 	sel_item last_selected;
 	/* The list we're building up to select with next */
 	sel_item to_select;
+	/* The table of objects */
+	sel_obj *obj_table;
+	unsigned int obj_used, obj_size;
 } sel_ctx;
+#define OBJ_TABLE_START	32
+#define IDX2TOKEN(idx) (NAL_SELECTOR_TOKEN)((unsigned int)idx | 0x8000)
+#define TOKEN2IDX(tok) ((unsigned int)tok & 0x7FFF)
+
+/* Helper functions for the object table */
+static void obj_table_zilch(sel_obj *items, unsigned int num)
+{
+	while(num--)
+		(items++)->what = 0;
+}
+static int obj_table_init(sel_ctx *ctx)
+{
+	ctx->obj_table = SYS_malloc(sel_obj, OBJ_TABLE_START);
+	if(!ctx->obj_table) return 0;
+	obj_table_zilch(ctx->obj_table, OBJ_TABLE_START);
+	ctx->obj_used = 0;
+	ctx->obj_size = OBJ_TABLE_START;
+	return 1;
+}
+static void obj_table_finish(sel_ctx *ctx)
+{
+	/* XXX: Should we warn when used>0? Probably */
+	SYS_free(sel_obj, ctx->obj_table);
+}
+static void obj_table_reset(sel_ctx *ctx)
+{
+	/* XXX: as with finish? */
+	ctx->obj_used = 0;
+}
+static int obj_table_add(sel_ctx *ctx)
+{
+	int loop = 0;
+	if(ctx->obj_used == ctx->obj_size) {
+		unsigned int newsize = ctx->obj_size * 3 / 2;
+		sel_obj *newitems = SYS_malloc(sel_obj, newsize);
+		if(!newitems) return -1;
+		obj_table_zilch(newitems, newsize);
+		if(ctx->obj_used)
+			SYS_memcpy_n(sel_obj, newitems, ctx->obj_table,
+					ctx->obj_used);
+		SYS_free(sel_obj, ctx->obj_table);
+		ctx->obj_table = newitems;
+		ctx->obj_size = newsize;
+	}
+	while(ctx->obj_table[loop].what != 0)
+		loop++;
+	ctx->obj_used++;
+	return loop;
+}
+static NAL_SELECTOR_TOKEN obj_table_add_listener(sel_ctx *ctx,
+					NAL_LISTENER *listener)
+{
+	int loc = obj_table_add(ctx);
+	if(loc < 0) return NAL_SELECTOR_TOKEN_NULL;
+	ctx->obj_table[loc].what = 2;
+	ctx->obj_table[loc].obj.listener = listener;
+	return IDX2TOKEN(loc);
+}
+static NAL_SELECTOR_TOKEN obj_table_add_connection(sel_ctx *ctx,
+					NAL_CONNECTION *conn)
+{
+	int loc = obj_table_add(ctx);
+	if(loc < 0) return NAL_SELECTOR_TOKEN_NULL;
+	ctx->obj_table[loc].what = 1;
+	ctx->obj_table[loc].obj.conn = conn;
+	return IDX2TOKEN(loc);
+}
+static void obj_table_del(sel_ctx *ctx, NAL_SELECTOR_TOKEN tok)
+{
+	unsigned int idx = TOKEN2IDX(tok);
+	assert(idx < ctx->obj_size);
+	assert(ctx->obj_table[idx].what != 0);
+	assert(ctx->obj_used > 0);
+	ctx->obj_table[idx].what = 0;
+	ctx->obj_used--;
+}
+static void obj_table_pre_select(sel_ctx *ctx)
+{
+	unsigned int loop = 0;
+	sel_obj *item = ctx->obj_table;
+	while(loop < ctx->obj_size) {
+		switch(item->what) {
+		case 1:
+			nal_connection_pre_select(item->obj.conn);
+			break;
+		case 2:
+			nal_listener_pre_select(item->obj.listener);
+			break;
+		default:
+			break;
+		}
+		item++;
+		loop++;
+	}
+}
+static void obj_table_post_select(sel_ctx *ctx)
+{
+	unsigned int loop = 0;
+	sel_obj *item = ctx->obj_table;
+	while(loop < ctx->obj_size) {
+		switch(item->what) {
+		case 1:
+			nal_connection_post_select(item->obj.conn);
+			break;
+		case 2:
+			nal_listener_post_select(item->obj.listener);
+			break;
+		default:
+			break;
+		}
+		item++;
+		loop++;
+	}
+}
 
 /**************************/
 /* predeclare our vtables */
 /**************************/
 
 /* Predeclare the selector functions */
-static int sel_on_create(NAL_SELECTOR *sel);
-static void sel_on_destroy(NAL_SELECTOR *sel);
-static void sel_on_reset(NAL_SELECTOR *sel);
-static NAL_SELECTOR_TYPE sel_get_type(const NAL_SELECTOR *sel);
-static void sel_fd_set(NAL_SELECTOR *sel, int fd, unsigned char flags);
-static void sel_fd_unset(NAL_SELECTOR *sel, int fd);
-static unsigned char sel_fd_test(const NAL_SELECTOR *sel, int fd);
-static void sel_fd_clear(NAL_SELECTOR *sel, int fd);
-static int sel_select(NAL_SELECTOR *sel, unsigned long usec_timeout, int use_timeout);
-/* This is extern'd in the nal_internal.h header, and used as the default vtable */
-const NAL_SELECTOR_vtable sel_fdselect_vtable = {
+static int sel_on_create(NAL_SELECTOR *);
+static void sel_on_destroy(NAL_SELECTOR *);
+static void sel_on_reset(NAL_SELECTOR *);
+static NAL_SELECTOR_TYPE sel_get_type(const NAL_SELECTOR *);
+static int sel_select(NAL_SELECTOR *, unsigned long usec_timeout, int use_timeout);
+static NAL_SELECTOR_TOKEN sel_add_listener(NAL_SELECTOR *, NAL_LISTENER *);
+static NAL_SELECTOR_TOKEN sel_add_connection(NAL_SELECTOR *, NAL_CONNECTION *);
+static void sel_del_listener(NAL_SELECTOR *, NAL_LISTENER *, NAL_SELECTOR_TOKEN);
+static void sel_del_connection(NAL_SELECTOR *, NAL_CONNECTION *, NAL_SELECTOR_TOKEN);
+static void sel_fd_set(NAL_SELECTOR *sel, NAL_SELECTOR_TOKEN,
+			int fd, unsigned char flags);
+static unsigned char sel_fd_test(const NAL_SELECTOR *sel, NAL_SELECTOR_TOKEN,
+			int fd);
+static const NAL_SELECTOR_vtable sel_fdselect_vtable = {
 	sizeof(sel_ctx),
 	sel_on_create,
 	sel_on_destroy,
 	sel_on_reset,
 	sel_get_type,
-	sel_fd_set,
-	sel_fd_unset,
-	sel_fd_test,
-	sel_fd_clear,
 	sel_select,
+	sel_add_listener,
+	sel_add_connection,
+	sel_del_listener,
+	sel_del_connection,
+	sel_fd_set,
+	sel_fd_test,
 };
+/* Expose this implementation */
+const NAL_SELECTOR_vtable *sel_fdselect(void)
+{
+	return &sel_fdselect_vtable;
+}
 
 /***************************************/
 /* Internal (sel_item) implementations */
@@ -116,18 +260,23 @@ static int nal_selector_item_select(sel_item *item,
 
 static int sel_on_create(NAL_SELECTOR *sel)
 {
-	/* ... because it works this way */
-	sel_on_reset(sel);
+	sel_ctx *ctx = nal_selector_get_vtdata(sel);
+	if(!obj_table_init(ctx)) return 0;
+	nal_selector_item_init(&ctx->last_selected);
+	nal_selector_item_init(&ctx->to_select);
 	return 1;
 }
 
 static void sel_on_destroy(NAL_SELECTOR *sel)
 {
+	sel_ctx *ctx = nal_selector_get_vtdata(sel);
+	obj_table_finish(ctx);
 }
 
 static void sel_on_reset(NAL_SELECTOR *sel)
 {
 	sel_ctx *ctx = nal_selector_get_vtdata(sel);
+	obj_table_reset(ctx);
 	nal_selector_item_init(&ctx->last_selected);
 	nal_selector_item_init(&ctx->to_select);
 }
@@ -137,7 +286,52 @@ static NAL_SELECTOR_TYPE sel_get_type(const NAL_SELECTOR *sel)
 	return NAL_SELECTOR_TYPE_FDSELECT;
 }
 
-static void sel_fd_set(NAL_SELECTOR *sel, int fd, unsigned char flags)
+static int sel_select(NAL_SELECTOR *sel, unsigned long usec_timeout,
+			int use_timeout)
+{
+	int res;
+	sel_ctx *ctx = nal_selector_get_vtdata(sel);
+	/* Pre-select */
+	obj_table_pre_select(ctx);
+	/* Migrate to_select over to last_selected */
+	nal_selector_item_flip(&ctx->last_selected, &ctx->to_select);
+	res = nal_selector_item_select(&ctx->last_selected, usec_timeout,
+				use_timeout);
+	/* Post-select */
+	if(res > 0) obj_table_post_select(ctx);
+	return res;
+}
+
+static NAL_SELECTOR_TOKEN sel_add_listener(NAL_SELECTOR *sel,
+				NAL_LISTENER *listener)
+{
+	sel_ctx *ctx = nal_selector_get_vtdata(sel);
+	return obj_table_add_listener(ctx, listener);
+}
+
+static NAL_SELECTOR_TOKEN sel_add_connection(NAL_SELECTOR *sel,
+				NAL_CONNECTION *conn)
+{
+	sel_ctx *ctx = nal_selector_get_vtdata(sel);
+	return obj_table_add_connection(ctx, conn);
+}
+
+static void sel_del_listener(NAL_SELECTOR *sel, NAL_LISTENER *listener,
+				NAL_SELECTOR_TOKEN token)
+{
+	sel_ctx *ctx = nal_selector_get_vtdata(sel);
+	obj_table_del(ctx, token);
+}
+
+static void sel_del_connection(NAL_SELECTOR *sel, NAL_CONNECTION *conn,
+				NAL_SELECTOR_TOKEN token)
+{
+	sel_ctx *ctx = nal_selector_get_vtdata(sel);
+	obj_table_del(ctx, token);
+}
+
+static void sel_fd_set(NAL_SELECTOR *sel, NAL_SELECTOR_TOKEN token,
+				int fd, unsigned char flags)
 {
 	sel_ctx *ctx = nal_selector_get_vtdata(sel);
 	if(flags & SELECTOR_FLAG_READ)
@@ -150,15 +344,8 @@ static void sel_fd_set(NAL_SELECTOR *sel, int fd, unsigned char flags)
 				(fd + 1) : ctx->to_select.max);
 }
 
-static void sel_fd_unset(NAL_SELECTOR *sel, int fd)
-{
-	sel_ctx *ctx = nal_selector_get_vtdata(sel);
-	FD_CLR2(fd, &ctx->to_select.reads);
-	FD_CLR2(fd, &ctx->to_select.sends);
-	FD_CLR2(fd, &ctx->to_select.excepts);
-}
-
-static unsigned char sel_fd_test(const NAL_SELECTOR *sel, int fd)
+static unsigned char sel_fd_test(const NAL_SELECTOR *sel,
+				NAL_SELECTOR_TOKEN token, int fd)
 {
 	unsigned char flags = 0;
 	sel_ctx *ctx = nal_selector_get_vtdata(sel);
@@ -171,22 +358,4 @@ static unsigned char sel_fd_test(const NAL_SELECTOR *sel, int fd)
 	return flags;
 }
 
-static void sel_fd_clear(NAL_SELECTOR *sel, int fd)
-{
-	sel_ctx *ctx = nal_selector_get_vtdata(sel);
-	FD_CLR2(fd, &ctx->last_selected.reads);
-	FD_CLR2(fd, &ctx->last_selected.sends);
-	FD_CLR2(fd, &ctx->last_selected.excepts);
-}
-
-static int sel_select(NAL_SELECTOR *sel, unsigned long usec_timeout,
-			int use_timeout)
-{
-	int res;
-	sel_ctx *ctx = nal_selector_get_vtdata(sel);
-	/* Migrate to_select over to last_selected */
-	nal_selector_item_flip(&ctx->last_selected, &ctx->to_select);
-	return nal_selector_item_select(&ctx->last_selected, usec_timeout,
-				use_timeout);
-}
-
+#endif
