0ad4e0363b33c8176c810308510637f28fc909da
geoffthorpe <geoffthorpe>
2004-05-08 03:00:05 +0000

    Add some timing/benchmarking support to nal_echo.

--- test/echo.c
+++ test/echo.c
@@ -25,12 +25,38 @@
 #include <libnal/nal.h>
 #include <libsys/post.h>
 
+/* TODO:
+ *   The use of the "traffic" variable in the main loop is a hack. This is not
+ *   really how much data that has been exchanged over the socket, nor even the
+ *   amount exchanged over the user/kernel boundary, but the amount
+ *   read/written into the NAL_CONNECTION buffers. Over time, this averages out
+ *   anyway, but it'd be better to support logging inside the NAL_CONNECTION
+ *   type itself and expose this instead.
+ */
+
 /* To monitor the number of accepted connections, define this */
 #define ECHO_DEBUG_CLIENTS
+
+typedef enum {
+	UNITS_BYTES,
+	UNITS_KILO,
+	UNITS_MEGA,
+	UNITS_GIGA
+} UNITS;
+static const char *UNITS_str[] = {
+	"bits", "kbits", "Mbits", "Gbits" };
+
+#define UNITS2STR(u)		UNITS_str[(u)]
 
 #define DEF_SERVER_ADDRESS	"UNIX:/tmp/foo"
 #define BUFFER_SIZE		(32*1024)
 #define MAX_CONNS		64
+#define DEF_UNITS		UNITS_BYTES
+
+/* Only support "-update" if we have the goodies */
+#if defined(HAVE_GETTIMEOFDAY) && defined(HAVE_GETRUSAGE)
+#define SUPPORT_UPDATE
+#endif
 
 static void usage(void)
 {
@@ -39,9 +65,33 @@ static void usage(void)
 	SYS_fprintf(SYS_stderr, "   -accept <addr>    - default='%s'\n", DEF_SERVER_ADDRESS);
 	SYS_fprintf(SYS_stderr, "   -max <num>        - default=%d\n", MAX_CONNS);
 	SYS_fprintf(SYS_stderr, "   -errinject <num>  - default=<none>\n");
+#ifdef SUPPORT_UPDATE
+	SYS_fprintf(SYS_stderr, "   -update <secs>    - default=<none>\n");
+	SYS_fprintf(SYS_stderr, "   -units <b,k,m,g>  - default='%s'\n", UNITS2STR(DEF_UNITS));
+	SYS_fprintf(SYS_stderr, "'units' displays traffic rates as bits, kilobits,\n");
+	SYS_fprintf(SYS_stderr, "megabits, or gigabits per second.\n");
+#endif
 	SYS_fprintf(SYS_stderr, "'errinject' will insert 0xdeadbeef into output every\n");
-	SYS_fprintf(SYS_stderr, "<num> times the selector logic breaks\n");
+	SYS_fprintf(SYS_stderr, "<num> times the selector logic breaks.\n");
 }
+
+#ifdef SUPPORT_UPDATE
+static int util_parseunits(const char *s, UNITS *u)
+{
+	if(strlen(s) != 1) goto err;
+	switch(*s) {
+	case 'b': *u = UNITS_BYTES; break;
+	case 'k': *u = UNITS_KILO; break;
+	case 'm': *u = UNITS_MEGA; break;
+	case 'g': *u = UNITS_GIGA; break;
+	default: goto err;
+	}
+	return 1;
+err:
+	SYS_fprintf(SYS_stderr, "Error, bad unit '%s'\n", s);
+	return 0;
+}
+#endif
 
 static int util_parsenum(const char *s, unsigned int *num)
 {
@@ -88,6 +138,16 @@ int main(int argc, char *argv[])
 	NAL_ADDRESS *addr;
 	NAL_LISTENER *listener;
 	NAL_SELECTOR *sel;
+#ifdef SUPPORT_UPDATE
+	unsigned int update = 0;
+	UNITS units = DEF_UNITS;
+	/* Timing variables for '-update' */
+	time_t tt1, tt2;
+	struct timeval tv1, tv2;
+	struct rusage ru1, ru2;
+	unsigned int traffic = 0;
+#endif
+
 	ARG_INC;
 	while(argc) {
 		if(strcmp(*argv, "-accept") == 0) {
@@ -106,6 +166,16 @@ int main(int argc, char *argv[])
 			ARG_CHECK("-errinject");
 			if(!util_parsenum(*argv, &errinject))
 				return 1;
+#ifdef SUPPORT_UPDATE
+		} else if(strcmp(*argv, "-update") == 0) {
+			ARG_CHECK("-update");
+			if(!util_parsenum(*argv, &update))
+				return 1;
+		} else if(strcmp(*argv, "-units") == 0) {
+			ARG_CHECK("-units");
+			if(!util_parseunits(*argv, &units))
+				return 1;
+#endif
 		} else
 			return err_unknown(*argv);
 		ARG_INC;
@@ -121,12 +191,55 @@ int main(int argc, char *argv[])
 	if(!NAL_ADDRESS_create(addr, str_addr, BUFFER_SIZE)) abort();
 	if(!NAL_LISTENER_create(listener, addr)) abort();
 	if(!NAL_LISTENER_add_to_selector(listener, sel)) abort();
+#ifdef SUPPORT_UPDATE
+	if(update) {
+		tt1 = time(NULL);
+		SYS_gettime(&tv1);
+		getrusage(RUSAGE_SELF, &ru1);
+		SYS_fprintf(SYS_stderr,
+"\n"
+"Note, '-update' statistics have accurate timing but the traffic measurements\n"
+"are based on transfers between user-space fifo buffers. As such, they should\n"
+"only be considered accurate \"on average\". Also, the traffic measured is\n"
+"two-way, identical traffic is passing in both directions so you can consider\n"
+"each direction to be half the advertised value.\n"
+"\n");
+#endif
+	}
 reselect:
 	tmp = NAL_SELECTOR_select(sel, 0, 0);
 	if(tmp <= 0) {
 		SYS_fprintf(SYS_stderr, "Error, NAL_SELECTOR_select() returned 0\n");
 		return 1;
 	}
+#ifdef SUPPORT_UPDATE
+	/* Check if an update is required */
+	if(update && ((tt2 = time(NULL)) >= (time_t)(tt1 + update))) {
+		unsigned long msecs, muser, msys;
+		double rate;
+		SYS_gettime(&tv2);
+		getrusage(RUSAGE_SELF, &ru2);
+		msecs = SYS_msecs_between(&tv1, &tv2);
+		muser = SYS_msecs_between(&ru1.ru_utime, &ru2.ru_utime);
+		msys = SYS_msecs_between(&ru1.ru_stime, &ru2.ru_stime);
+		rate = 2000.0 * traffic / (double)msecs;
+		switch(units) {
+		case UNITS_GIGA: rate /= 1024;
+		case UNITS_MEGA: rate /= 1024;
+		case UNITS_KILO: rate /= 1024;
+		case UNITS_BYTES: break;
+		default: abort(); /* bug */
+		}
+		SYS_fprintf(SYS_stdout, "Update: %ld msecs elapsed, %.2f %s/s, "
+			"%.1f%% user, %.1f%% kernel\n", msecs, rate,
+			UNITS2STR(units), (100.0 * muser)/((float)msecs),
+			(100.0 * msys)/((float)msecs));
+		tt1 = tt2;
+		SYS_timecpy(&tv1, &tv2);
+		SYS_memcpy(struct rusage, &ru1, &ru2);
+		traffic = 0;
+	}
+#endif
 	while((conns_used < num_conns) && NAL_CONNECTION_accept(conn[conns_used],
 						listener)) {
 		if(!NAL_CONNECTION_add_to_selector(conn[conns_used], sel))
@@ -170,7 +283,7 @@ reselect:
 						sizeof(deadbeef));
 					inject = 0;
 				}
-				NAL_BUFFER_transfer(buf_send, buf_read, 0);
+				traffic += NAL_BUFFER_transfer(buf_send, buf_read, 0);
 			}
 			loop++;
 		}
